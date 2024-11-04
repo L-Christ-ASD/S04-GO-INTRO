7e32998be63bace82ed1bfaa4455efd2df439db0
L-Christ-ASD <christ.devops75@gmail.com>
2024-11-04 13:30:37 +0000

    my first commit on langage GO

---
+++ go.mod
@@ -0,0 +1,3 @@
+module IntroGo
+
+go 1.22.7
--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
+
+func add(a int, b int) int{
+	return a + b
+}
+
+//var somme int. ou alors := pour déclarer et assigner en même temps une variable!
 
 func main() {
 	fmt.Println("hello world")
+	somme := add(3, 4)
+	fmt.Printf("La somme de 3 et 4 est : %v\n", somme)
+	fmt.Printf("La multiplication de 3 et 4 est : %v\n", multiplication(3, 4))
+	resultat, err := division(3, 0)
+	if err != nil {
+		log.Fatalf("alerte, division par zéro: %v", err)
+	}
+	log.Printf("La division de 3 et 4 est : %v\n", resultat)
+	
 }
